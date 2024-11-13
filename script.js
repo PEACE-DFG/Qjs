1972a9a91e721e7ea1a62da2a91ba7ecc00af80d
PEACE-DFG <awofesobipeace@gmail.com>
2024-11-13 10:55:23 +0100

    class started

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log('I am an external js file')
+document.getElementById('first').innerHTML='This is the first js class using the DOM method';
+
+let x=5;
+let y=10;
+
+let z= x+y;
+
+console.log(z)
+
+console.log(2===3)
+
+w=5;
+l=50;
+
+console.log(w==l)
+
+
+let cars=['Toyota','BMW','Lambo','Ferari']
+
+console.log(cars)
+console.log(cars[2])
+
+
+let person={
+  firstName:'Qudus',
+  secondName:'Abdul',
+  age:'50 years old'
+}
+let comp = 2<3
+let real = 'i am real'
+
+console.log(person.age)
+
+document.getElementById('second').innerHTML='My Javascript Class';
+document.getElementById('text').innerHTML='In my javascript class , i learnt about so many things'
+console.log(typeof(w))
+console.log(typeof(cars))
+console.log(typeof(real))
+console.log(typeof(comp))
+
